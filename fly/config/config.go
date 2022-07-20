375d0a1aa94c5dd3c09bfc676aeba585cecf0d80
subham sarkar <80461438+subham-deepsource@users.noreply.github.com>
2022-07-21 01:54:04 +0530

    Update config.go

--- fly/config/config.go
+++ fly/config/config.go
@@ -11,9 +11,7 @@ func OverrideTaskParams(configFile []byte, args []string) (atc.TaskConfig, error
 	if err != nil {
 		return atc.TaskConfig{}, err
 	}
-
 	config.Run.Args = append(config.Run.Args, args...)
-
 	for k := range config.Params {
 		env, found := syscall.Getenv(k)
 		if found {
